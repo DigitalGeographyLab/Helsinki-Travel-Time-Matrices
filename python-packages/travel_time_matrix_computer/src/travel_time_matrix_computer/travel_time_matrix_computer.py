9e02537f7869684ddde171a24b8015f2f6c48041
Christoph Fink <christoph.fink@gmail.com>
2023-04-04 11:52:41 +0300

    Add some debug print statements to see progress while testing

--- python-packages/travel_time_matrix_computer/src/travel_time_matrix_computer/travel_time_matrix_computer.py
+++ python-packages/travel_time_matrix_computer/src/travel_time_matrix_computer/travel_time_matrix_computer.py
@@ -21,22 +21,26 @@ class TravelTimeMatrixComputer:
         self._kwargs = kwargs
 
     def run(self):
+        print("now computing walking times")
         walking_travel_time_matrix_computer = WalkingTravelTimeMatrixComputer(
             **self._kwargs
         )
         travel_times = walking_travel_time_matrix_computer.run()
         del walking_travel_time_matrix_computer
 
+        print("now computing cycling times")
         cycling_travel_time_matrix_computer = CyclingTravelTimeMatrixComputer(
             **self._kwargs
         )
         travel_times = travel_times.join(cycling_travel_time_matrix_computer.run())
         del cycling_travel_time_matrix_computer
 
+        print("now computing driving times")
         car_travel_time_matrix_computer = CarTravelTimeMatrixComputer(**self._kwargs)
         travel_times = travel_times.join(car_travel_time_matrix_computer.run())
         del car_travel_time_matrix_computer
 
+        print("now computing public transport times")
         public_transport_travel_time_matrix_computer = (
             PublicTransportTravelTimeMatrixComputer(**self._kwargs)
         )
