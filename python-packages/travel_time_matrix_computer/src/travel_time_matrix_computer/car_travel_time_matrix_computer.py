dcdf607f313a46a8f6f0c96c2dfd04beb8c04029
Christoph Fink <christoph.fink@gmail.com>
2023-05-12 14:46:22 +0300

    disable Perola’s car speeds

--- python-packages/travel_time_matrix_computer/src/travel_time_matrix_computer/car_travel_time_matrix_computer.py
+++ python-packages/travel_time_matrix_computer/src/travel_time_matrix_computer/car_travel_time_matrix_computer.py
@@ -54,21 +54,21 @@ class CarTravelTimeMatrixComputer(
 
     def run(self):
         travel_times = None
-        original_osm_extract_file = self.osm_extract_file
+        # original_osm_extract_file = self.osm_extract_file
 
         for timeslot_name, timeslot_time in self.DEPARTURE_TIMES.items():
             column_suffix = timeslot_name[0]
 
-            annotated_osm_extract_file = (
-                original_osm_extract_file.parent
-                / f"{self.osm_extract_file.stem}_{timeslot_name}.osm.pbf"
-            )
+            # annotated_osm_extract_file = (
+            #     original_osm_extract_file.parent
+            #     / f"{self.osm_extract_file.stem}_{timeslot_name}.osm.pbf"
+            # )
 
-            car_speed_annotator.CarSpeedAnnotator(timeslot_name).annotate(
-                self.osm_extract_file,
-                annotated_osm_extract_file,
-            )
-            self.osm_extract_file = annotated_osm_extract_file
+            # car_speed_annotator.CarSpeedAnnotator(timeslot_name).annotate(
+            #     self.osm_extract_file,
+            #     annotated_osm_extract_file,
+            # )
+            # self.osm_extract_file = annotated_osm_extract_file
 
             if self.calculate_distances:
                 detailed_itineraries_computer = r5py.DetailedItinerariesComputer(
@@ -115,7 +115,7 @@ class CarTravelTimeMatrixComputer(
             else:
                 travel_times = travel_times.join(_travel_times)
 
-            annotated_osm_extract_file.unlink()
-            self.osm_extract_file = original_osm_extract_file
+            # annotated_osm_extract_file.unlink()
+            # self.osm_extract_file = original_osm_extract_file
 
         return travel_times
